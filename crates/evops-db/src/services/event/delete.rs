2aaa8fd392b8f337a880331d6f30d97bd74e2929
Egor Pustovoytenko <puseg2006@gmail.com>
2025-07-17 17:35:26 +0300

    wip

--- crates/evops-db/src/services/event/delete.rs
+++ crates/evops-db/src/services/event/delete.rs
@@ -1,15 +1,55 @@
-use diesel::{BelongingToDsl as _, QueryResult};
-use diesel::{QueryDsl as _, SelectableHelper as _};
+use diesel::ExpressionMethods as _;
+use diesel::QueryDsl as _;
+use diesel_async::AsyncConnection as _;
 use diesel_async::AsyncPgConnection;
 use diesel_async::RunQueryDsl as _;
+use diesel_async::scoped_futures::ScopedFutureExt as _;
 
 use evops_models::{ApiError, ApiResult};
 
-use crate::models;
 use crate::schema;
 
 impl crate::Database {
-    pub async fn delete_event(&mut self, _id: evops_models::EventId) -> ApiResult<()> {
-        todo!();
+    pub async fn delete_event(&mut self, id: evops_models::EventId) -> ApiResult<()> {
+        self.conn
+            .transaction(|conn| {
+                async {
+                    unsafe { Self::delete_events_to_tags(conn, id) }.await?;
+                    unsafe { Self::delete_event_table(conn, id) }.await
+                }
+                .scope_boxed()
+            })
+            .await
+    }
+
+    async unsafe fn delete_events_to_tags(
+        conn: &mut AsyncPgConnection,
+        id: evops_models::EventId,
+    ) -> ApiResult<()> {
+        diesel::delete(
+            schema::events_to_tags::table
+                .filter(schema::events_to_tags::event_id.eq(id.into_inner())),
+        )
+        .execute(conn)
+        .await
+        .map_err(|e| match e {
+            diesel::result::Error::NotFound => ApiError::NotFound(e.to_string()),
+            _ => e.into(),
+        })?;
+        Ok(())
+    }
+
+    async unsafe fn delete_event_table(
+        conn: &mut AsyncPgConnection,
+        id: evops_models::EventId,
+    ) -> ApiResult<()> {
+        diesel::delete(schema::events::table.find(id.into_inner()))
+            .execute(conn)
+            .await
+            .map_err(|e| match e {
+                diesel::result::Error::NotFound => ApiError::NotFound(e.to_string()),
+                _ => e.into(),
+            })?;
+        Ok(())
     }
 }
