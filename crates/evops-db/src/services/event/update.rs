bbfd6e44df597f06a6e5f738ec00bfb433cdb8c5
Egor Pustovoytenko <puseg2006@gmail.com>
2025-07-17 23:41:29 +0300

    implement CRUD

--- crates/evops-db/src/services/event/update.rs
+++ crates/evops-db/src/services/event/update.rs
@@ -1,15 +1,115 @@
-use diesel::{BelongingToDsl as _, QueryResult};
-use diesel::{QueryDsl as _, SelectableHelper as _};
+use diesel::ExpressionMethods as _;
+use diesel::QueryDsl as _;
+use diesel_async::AsyncConnection as _;
 use diesel_async::AsyncPgConnection;
 use diesel_async::RunQueryDsl as _;
+use diesel_async::scoped_futures::ScopedFutureExt as _;
 
 use evops_models::{ApiError, ApiResult};
 
-use crate::models;
 use crate::schema;
 
 impl crate::Database {
-    pub async fn update_event(&mut self, _form: evops_models::UpdateEventForm) -> ApiResult<()> {
-        todo!();
+    pub async fn update_event(
+        &mut self,
+        id: evops_models::EventId,
+        form: evops_models::UpdateEventForm,
+    ) -> ApiResult<()> {
+        self.conn
+            .transaction(|conn| {
+                async {
+                    if form.description.is_some()
+                        || form.title.is_some()
+                        || form.track_attendance.is_some()
+                    {
+                        unsafe { Self::update_basic_fields(conn, id, &form) }.await?;
+                    }
+                    if let Some(tag_ids) = form.tag_ids {
+                        unsafe { Self::delete_tags_for_event(conn, id) }.await?;
+                        unsafe { Self::create_tags_for_event(conn, id, tag_ids) }.await?;
+                    }
+                    ApiResult::Ok(())
+                }
+                .scope_boxed()
+            })
+            .await?;
+        Ok(())
+    }
+
+    async unsafe fn update_basic_fields(
+        conn: &mut AsyncPgConnection,
+        id: evops_models::EventId,
+        form: &evops_models::UpdateEventForm,
+    ) -> ApiResult<()> {
+        diesel::update(schema::events::table.find(id.into_inner()))
+            .set({
+                let description_eq = {
+                    form.description
+                        .as_ref()
+                        .map(|it| schema::events::description.eq(it.as_ref()))
+                };
+                let title_eq = {
+                    form.title
+                        .as_ref()
+                        .map(|it| schema::events::title.eq(it.as_ref()))
+                };
+                let track_attendance_eq = {
+                    form.track_attendance
+                        .map(|it| schema::events::with_attendance.eq(it))
+                };
+                (description_eq, title_eq, track_attendance_eq)
+            })
+            .execute(conn)
+            .await
+            .map_err(|e| match e {
+                diesel::result::Error::NotFound => ApiError::NotFound(e.to_string()),
+                _ => e.into(),
+            })?;
+        Ok(())
+    }
+
+    async unsafe fn delete_tags_for_event(
+        conn: &mut AsyncPgConnection,
+        id: evops_models::EventId,
+    ) -> ApiResult<()> {
+        diesel::delete({
+            schema::events_to_tags::table
+                .filter(schema::events_to_tags::event_id.eq(id.into_inner()))
+        })
+        .execute(conn)
+        .await
+        .map_err(|e| match e {
+            diesel::result::Error::NotFound => ApiError::NotFound(e.to_string()),
+            _ => e.into(),
+        })?;
+        Ok(())
+    }
+
+    async unsafe fn create_tags_for_event(
+        conn: &mut AsyncPgConnection,
+        id: evops_models::EventId,
+        new_tags: evops_models::EventTagIds,
+    ) -> ApiResult<()> {
+        let records: Vec<_> = {
+            new_tags
+                .into_inner()
+                .into_iter()
+                .map(|t| {
+                    (
+                        schema::events_to_tags::event_id.eq(id.into_inner()),
+                        schema::events_to_tags::tag_id.eq(t.into_inner()),
+                    )
+                })
+                .collect()
+        };
+        diesel::insert_into(schema::events_to_tags::table)
+            .values(&records)
+            .execute(conn)
+            .await
+            .map_err(|e| match e {
+                diesel::result::Error::NotFound => ApiError::NotFound(e.to_string()),
+                _ => e.into(),
+            })?;
+        Ok(())
     }
 }
--- crates/evops-db/src/services/tag/delete.rs
+++ crates/evops-db/src/services/tag/delete.rs
@@ -1,15 +1,45 @@
-use diesel::{BelongingToDsl as _, GroupedBy as _, QueryResult};
-use diesel::{ExpressionMethods as _, QueryDsl as _, SelectableHelper as _};
-use diesel_async::AsyncPgConnection;
-use diesel_async::RunQueryDsl as _;
+use diesel::{ExpressionMethods as _, QueryDsl as _};
+use diesel_async::{
+    AsyncConnection as _, AsyncPgConnection, RunQueryDsl as _, scoped_futures::ScopedFutureExt as _,
+};
 
 use evops_models::{ApiError, ApiResult};
 
-use crate::models;
 use crate::schema;
 
 impl crate::Database {
     pub async fn delete_tag(&mut self, id: evops_models::TagId) -> ApiResult<()> {
-        todo!();
+        self.conn
+            .transaction(|conn| {
+                async { unsafe { Self::delete_tag_unatomic(conn, id) }.await }.scope_boxed()
+            })
+            .await
+    }
+
+    async unsafe fn delete_tag_unatomic(
+        conn: &mut AsyncPgConnection,
+        id: evops_models::TagId,
+    ) -> ApiResult<()> {
+        diesel::delete({
+            schema::tag_aliases::table.filter(schema::tag_aliases::tag_id.eq(id.into_inner()))
+        })
+        .execute(conn)
+        .await?;
+
+        diesel::delete({
+            schema::events_to_tags::table.filter(schema::events_to_tags::tag_id.eq(id.into_inner()))
+        })
+        .execute(conn)
+        .await?;
+
+        diesel::delete(schema::tags::table.find(id.into_inner()))
+            .execute(conn)
+            .await
+            .map_err(|e| match e {
+                diesel::result::Error::NotFound => ApiError::NotFound(e.to_string()),
+                _ => e.into(),
+            })?;
+
+        Ok(())
     }
 }
