73d94e165ef3d53c7d4b11d8721eb75e4d05e4d3
Asqar Arslanov <asqarslanov@gmail.com>
2025-07-19 17:45:22 +0300

    .

--- crates/evops-db/src/services/user/create.rs
+++ crates/evops-db/src/services/user/create.rs
@@ -1,5 +1,8 @@
 use diesel::{Insertable, SelectableHelper as _};
+use diesel_async::AsyncConnection as _;
+use diesel_async::AsyncPgConnection;
 use diesel_async::RunQueryDsl as _;
+use diesel_async::scoped_futures::ScopedFutureExt as _;
 use uuid::Uuid;
 
 use evops_models::ApiResult;
@@ -13,29 +16,58 @@ use crate::schema;
 struct NewUser<'a> {
     id: Uuid,
     user_login: &'a str,
+    password_argon2: &'a str,
     display_name: &'a str,
 }
 
 impl crate::Database {
-    pub async fn create_user(
+    pub async fn sign_up_user(
         &mut self,
-        form: evops_models::NewUserForm,
-    ) -> ApiResult<evops_models::UserId> {
-        // This function doesn't start a transaction
-        // because there's only one insert,* where it can fail.
+        user_id: evops_models::UserId,
+        login: &evops_models::UserLogin,
+        password_hash: &evops_models::UserPasswordHash,
+        display_name: &evops_models::UserDisplayName,
+        refresh_token: &evops_models::JsonWebToken,
+    ) -> ApiResult<()> {
+        self.conn
+            .transaction(|conn| {
+                async {
+                    unsafe {
+                        Self::sign_up_user_unatomic(
+                            conn,
+                            user_id,
+                            login,
+                            password_hash,
+                            display_name,
+                            refresh_token,
+                        )
+                        .await
+                    }
+                }
+                .scope_boxed()
+            })
+            .await
+    }
 
-        let user_id = evops_models::UserId::new(Uuid::now_v7());
-
+    pub async unsafe fn sign_up_user_unatomic(
+        conn: &mut AsyncPgConnection,
+        user_id: evops_models::UserId,
+        login: &evops_models::UserLogin,
+        password_hash: &evops_models::UserPasswordHash,
+        display_name: &evops_models::UserDisplayName,
+        refresh_token: &evops_models::JsonWebToken,
+    ) -> ApiResult<()> {
         diesel::insert_into(schema::users::table)
             .values(self::NewUser {
                 id: user_id.into_inner(),
-                user_login: form.login.as_ref(),
-                display_name: form.display_name.as_ref(),
+                user_login: login.as_ref(),
+                password_argon2: password_hash.as_ref(),
+                display_name: display_name.as_ref(),
             })
             .returning(models::User::as_returning())
-            .execute(&mut self.conn)
-            .await?; // *
+            .execute(conn)
+            .await?;
 
-        Ok(user_id)
+        Ok(())
     }
 }
